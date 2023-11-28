fdee05acb5b56436ac76a1550784ef851a8b5314
Yared91 <ykebede010@gmail.com>
2023-11-28 07:22:18 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 # Author - Tolulope Fakunle
 for number1 in range(0, 10):
     for number2 in range(number1 + 1, 10):
-        if number1 == 5 and number2 == 6:
+        if number1 == 8 and number2 == 9:
             print("{}{}".format(number1, number2))
         else:
             print("{}{}".format(number1, number2), end=", ")
