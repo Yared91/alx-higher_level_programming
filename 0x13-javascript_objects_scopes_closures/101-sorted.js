fce5be2ad092ae230fbaa30c374922fae60e732d
Yared91 <ykebede010@gmail.com>
2024-03-12 10:59:40 -0700

    update

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,9 +4,9 @@ const dict = require('./101-data').dict;
 
 let newDic = {};
 for (let key in dict) {
-  if (newDic[dict[key]] === undefined) {
-    newDic[dict[key]] = [];
-  }
+	if (newDic[dict[key]] === undefined) {
+		newDic[dict[key]] = [];
+	}
 	newDic[dict[key]].push(key);
 }
 
