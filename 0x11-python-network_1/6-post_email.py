5fee2b77c081616096a92785713d308970830629
Yared91 <ykebede010@gmail.com>
2024-03-29 05:13:48 -0700

    6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""sends a POST request to the passed URL with the email as a parameter"""
+import requests
+from sys import argv
+
+
+def send_post_request(url ,data):
+    """sends a POST request with url and email"""
+    order = requests.post(url, data=data)
+    print(f"{order.text}")
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    main = {"email": argv[2]}
+    send_post_request(url ,main)
