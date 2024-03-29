43282949e6d0a3b367047e9f74634f3819bfe0f2
Yared91 <ykebede010@gmail.com>
2024-03-29 03:39:53 -0700

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,13 +5,12 @@ import urllib.request
 
 def fetch_status():
     """fetches https://alx-intranet.hbtn.io/status"""
-
     try:
         order = urllib.request.Request("https://alx-intranet.hbtn.io/status")
         with urllib.request.urlopen(order) as response:
             main = response.read()
             print("Body response:")
-            print(f"\t- type: {type{main}}")
+            print(f"\t- type: {type(main)}")
             print(f"\t- content: {main}")
             print(f"\t- utf8 content: {main.decode('utf-8')}")
     except urllib.error.URLError as e:
---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""sends a POST request to the passed URL with the email as a parameter."""
+import urllib.request
+import urllib.parse
+from sys import argv
+
+
+def post_request(url, email):
+    values = {'email': email}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    order = urllib.request.Request(url, datat)
+
+    with urllib.request.urlopen(order) as response:
+        order_con = response.read().decode('utf-8')
+        print(order_con)
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    post_request(url, email)
