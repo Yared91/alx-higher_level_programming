e4a697fb4bb2e4d6316bfed463488310bb4cc51f
Yared91 <ykebede010@gmail.com>
2024-03-29 10:44:31 -0700

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""sends a POST request to http://0.0.0.0:5000/search_user as a parameter"""
+import requests
+from sys import argv
+
+
+def post_request(letter):
+    """sends post request"""
+    payload = {"q": letter}
+    order = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+
+    try:
+        main = order.json()
+        if main == {}:
+            print(f"{No result}")
+        else:
+            print(f"[{main.get('id')}] {main.get('name')}")
+    except ValueError:
+        print(f"{Not a valid JSON}")
+
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    post_request(letter)
