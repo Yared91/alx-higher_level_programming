aee84cf5c03420721681f94e21350661a811a5d0
Yared91 <ykebede010@gmail.com>
2024-04-23 04:33:29 -0700

    update

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,21 +5,33 @@ const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
+    if (error) {
+        console.log(error);
+    } else {
+        const content = JSON.parse(body);
+        const characters = content.characters;
+
+        const characterPromises = characters.map(character => {
+            return new Promise((resolve, reject) => {
+                request.get(character, (error, response, body) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        const names = JSON.parse(body);
+                        resolve(names.name);
+                    }
+                });
+            });
+        });
+
+        Promise.all(characterPromises)
+            .then(names => {
+                names.forEach(name => {
+                    console.log(name);
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
-  }
 });
